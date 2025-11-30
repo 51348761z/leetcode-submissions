562e8bdd250e86d7942e79344728e0e30f91d19f
51348761z <hl_wong@st.gxu.edu.cn>
2025-11-30 03:24:58 +0000

    [LeetCode Sync] Runtime - 5 ms (47.46%), Memory - 59 MB (5.01%)

--- my-folder/0001-two-sum/solution.js
+++ my-folder/0001-two-sum/solution.js
@@ -4,13 +4,12 @@
  * @return {number[]}
  */
 var twoSum = function (nums, target) {
-  let map = new Map();
-  for (const [i, n] of nums.entries()) {
-    const complement = target - n;
+  const map = new Map();
+  for (const [index, num] of nums.entries()) {
+    const complement = target - num;
     if (map.has(complement)) {
-      return [i, map.get(complement)];
+      return [map.get(complement), index];
     }
-    map.set(n, i);
+    map.set(num, index);
   }
-  return [];
 };
