002d7d1cc2418bfb4d98469a6dca04fd846bfa00
51348761z <hl_wong@st.gxu.edu.cn>
2025-12-04 08:56:26 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18.2 MB (100.00%)

---
+++ my-folder/0033-search-in-rotated-sorted-array/solution.py
@@ -0,0 +1,26 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        res = -1
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            m = l + ((r - l) >> 1)
+            if target == nums[m]:
+                res = m
+                break
+
+            # left sorted portion
+            if nums[m] >= nums[l]:
+                if target > nums[m] or target < nums[l]:
+                    l = m + 1
+                else:
+                    r = m - 1
+            # right sorted portion
+            else:
+                if target > nums[r] or target < nums[m]:
+                    r = m - 1
+                else:
+                    l = m + 1
+
+        return res
+
