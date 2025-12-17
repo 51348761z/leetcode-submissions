ea1647f9a9cd6db305b34e87e517a525a5d1eb3c
51348761z <hl_wong@st.gxu.edu.cn>
2025-12-17 18:06:36 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.1 MB (99.99%)

--- my-folder/0001-two-sum/solution.py
+++ my-folder/0001-two-sum/solution.py
@@ -1,17 +1,12 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        indices = {}
+import enum
 
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
         for i, n in enumerate(nums):
-            diff = target - n
-            if diff in indices:
-                return [indices[diff], i]
-            indices[n] = i
-
+            if target - n in seen:
+                return [i, seen.get(target - n, 0)]
+            seen[n] = i
         return []
 
