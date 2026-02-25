bd69571bcb6ed3665b1c504ab595ae92a6495856
51348761z <hl_wong@st.gxu.edu.cn>
2026-02-25 04:30:54 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 57.6 MB (26.54%)

--- my-folder/0001-two-sum/solution.ts
+++ my-folder/0001-two-sum/solution.ts
@@ -1,12 +1,13 @@
 function twoSum(nums: number[], target: number): number[] {
-  const map = new Map<number, number>();
+  const numIdxMap = new Map<number, number>();
+
   for (let i = 0; i < nums.length; i++) {
-    const n = nums[i];
-    const diff = target - n;
-    if (map.has(diff)) {
-      return [map.get(diff)!, i];
+    const diff = target - nums[i];
+    if (numIdxMap.has(diff)) {
+      return [i, numIdxMap.get(diff)!];
     }
-    map.set(n, i);
+    numIdxMap.set(nums[i], i);
   }
+
   return [];
 }
