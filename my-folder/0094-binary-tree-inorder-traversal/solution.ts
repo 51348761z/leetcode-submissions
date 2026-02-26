a2de5b5dd22e3b12ce30b8124aee8a75df60c76a
51348761z <hl_wong@st.gxu.edu.cn>
2026-02-26 02:34:18 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 55.2 MB (58.75%)

---
+++ my-folder/0094-binary-tree-inorder-traversal/solution.ts
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function inorderTraversal(root: TreeNode | null): number[] {
+  if (!root) return [];
+  let curr: TreeNode | null = root;
+  const stack: TreeNode[] = [];
+  const res = [];
+  while (curr || stack.length > 0) {
+    while (curr) {
+      stack.push(curr);
+      curr = curr.left;
+    }
+
+    curr = stack.pop()!;
+    res.push(curr.val);
+
+    curr = curr.right;
+  }
+  return res;
+}
