6d894bbb18fde954cef38961084919c99f567dad
51348761z <hl_wong@st.gxu.edu.cn>
2026-03-04 10:17:04 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.4 MB (32.72%)

--- my-folder/0461-hamming-distance/solution.py
+++ my-folder/0461-hamming-distance/solution.py
@@ -1,6 +1,7 @@
 class Solution:
     def hammingDistance(self, x: int, y: int) -> int:
-        dist, xor = 0, x ^ y
+        dist = 0
+        xor = x ^ y
         while xor:
             xor &= xor - 1
             dist += 1
