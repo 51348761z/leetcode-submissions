128ab40a4e3782b72d70882619640340e2fd9b36
51348761z <hl_wong@st.gxu.edu.cn>
2026-03-05 17:53:55 +0000

    [LeetCode Sync] Runtime - 1 ms (5.71%), Memory - 19.3 MB (77.76%)

--- my-folder/0017-letter-combinations-of-a-phone-number/solution.py
+++ my-folder/0017-letter-combinations-of-a-phone-number/solution.py
@@ -1,26 +1,18 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         res = []
-        digitToChar = {
-            "2": "abc",
-            "3": "def",
-            "4": "ghi",
-            "5": "jkl",
-            "6": "mno",
-            "7": "pqrs",
-            "8": "tuv",
-            "9": "wxyz",
+        numberToLatters = {
+            '2': "abc", '3': "def", '4': "ghi", '5': "jkl", '6': "mno", '7': "pqrs", '8': "tuv", '9': "wxyz"
         }
 
         def backtracking(i, curStr):
             if len(curStr) == len(digits):
                 res.append(curStr)
                 return
-            for c in digitToChar[digits[i]]:
-                backtracking(i + 1, curStr + c)
+            
+            for c in numberToLatters[digits[i]]:
+                backtracking(i+1, curStr+c)
 
-        if digits:
-            backtracking(0, "")
+        backtracking(0, "")
         return res
-
 
