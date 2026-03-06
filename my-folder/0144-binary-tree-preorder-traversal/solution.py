29c86cbaacf40b7f1edbce01ccd89b660a21d148
51348761z <hl_wong@st.gxu.edu.cn>
2026-03-06 18:43:02 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.3 MB (68.54%)

--- my-folder/0144-binary-tree-preorder-traversal/solution.py
+++ my-folder/0144-binary-tree-preorder-traversal/solution.py
@@ -8,16 +8,18 @@ class TreeNode:
 
 class Solution:
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        res = []
 
-        def dfs(root):
-            if not root:
-                return
-            res.append(root.val)
-            dfs(root.left)
-            dfs(root.right)
+        res, stack = [], []
+        curr = root
 
-        dfs(root)
+        while curr or stack:
+            if curr:
+                res.append(curr.val)
+                stack.append(curr.right)
+                curr = curr.left
+            else:
+                curr = stack.pop()
+
         return res
 
 
