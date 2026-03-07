361155ec1da2acec1ddc5008d078f5b02a2c1b1b
51348761z <hl_wong@st.gxu.edu.cn>
2026-03-07 18:36:54 +0000

    [LeetCode Sync] Runtime - 4 ms (59.49%), Memory - 20.6 MB (24.36%)

--- my-folder/0283-move-zeroes/solution.py
+++ my-folder/0283-move-zeroes/solution.py
@@ -4,9 +4,9 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         i = 0
-        for j in range(i, len(nums)):
-            if nums[j] != 0:
-                nums[j], nums[i] = nums[i], nums[j]
+        for j in range(len(nums)):
+            if nums[j]:
+                nums[i], nums[j] = nums[j], nums[i]
                 i += 1
 
 
