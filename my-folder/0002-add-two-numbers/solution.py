746f9f8a0871f910acafcffb5b794d0a6e7f62b3
51348761z <hl_wong@st.gxu.edu.cn>
2026-03-08 21:21:22 +0000

    [LeetCode Sync] Runtime - 3 ms (64.88%), Memory - 19.4 MB (10.71%)

--- my-folder/0002-add-two-numbers/solution.py
+++ my-folder/0002-add-two-numbers/solution.py
@@ -9,9 +9,10 @@ class Solution:
     def addTwoNumbers(
         self, l1: Optional[ListNode], l2: Optional[ListNode]
     ) -> Optional[ListNode]:
-        dummy = ListNode()
+        dummy = ListNode(0)
         curr = dummy
         carry = 0
+
         while l1 or l2 or carry:
             v1 = l1.val if l1 else 0
             v2 = l2.val if l2 else 0
@@ -24,6 +25,7 @@ class Solution:
             curr = curr.next
             l1 = l1.next if l1 else None
             l2 = l2.next if l2 else None
+
         return dummy.next
 
 
