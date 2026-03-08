0b36b062b1de3dbddd36c0600ca72c607d2ca75f
51348761z <hl_wong@st.gxu.edu.cn>
2026-03-08 20:40:04 +0000

    [LeetCode Sync] Runtime - 20 ms (62.31%), Memory - 25.4 MB (69.77%)

--- my-folder/0238-product-of-array-except-self/solution.py
+++ my-folder/0238-product-of-array-except-self/solution.py
@@ -1,19 +1,17 @@
-
-
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        products = [1] * (len(nums))
+        res = [1] * len(nums)
 
         prefix = 1
         for i, n in enumerate(nums):
-            products[i] = prefix
+            res[i] = prefix
             prefix *= n
 
         postfix = 1
         for i in range(len(nums) - 1, -1, -1):
-            products[i] *= postfix
+            res[i] *= postfix
             postfix *= nums[i]
 
-        return products
+        return res
 
 
