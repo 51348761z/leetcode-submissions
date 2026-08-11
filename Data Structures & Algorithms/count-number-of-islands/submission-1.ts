63f3bfd5ceaec8163f133826d76350c083d7e8e8
51348761z <851315514@qq.com>
2026-08-12 02:54:14 +0800

    Add: count-number-of-islands - submission-1

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-1.ts
@@ -0,0 +1,50 @@
+class Solution {
+  /**
+   * @param {character[][]} grid
+   * @return {number}
+   */
+  numIslands(grid: string[][]): number {
+    const rows = grid.length;
+    const cols = grid[0].length;
+    const visited = Array.from({ length: rows }, () => Array(cols).fill(false));
+    let islandCount = 0;
+    const isInsideGrid = (row: number, col: number) =>
+      row >= 0 && row < rows && col >= 0 && col < cols;
+    const directions = [
+      [-1, 0],
+      [1, 0],
+      [0, -1],
+      [0, 1],
+    ] as const;
+
+    for (let r = 0; r < rows; ++r) {
+      for (let c = 0; c < cols; ++c) {
+        if (grid[r][c] !== "1" || visited[r][c]) continue;
+
+        islandCount++;
+        visited[r][c] = true;
+        const stack: [number, number][] = [[r, c]];
+
+        while (stack.length > 0) {
+          const [currentRow, currentCol] = stack.pop();
+          for (const [rowChange, colChange] of directions) {
+            const nextRow = currentRow + rowChange;
+            const nextCol = currentCol + colChange;
+
+            if (
+              !isInsideGrid(nextRow, nextCol) ||
+              grid[nextRow][nextCol] !== "1" ||
+              visited[nextRow][nextCol]
+            )
+              continue;
+
+            visited[nextRow][nextCol] = true;
+            stack.push([nextRow, nextCol]);
+          }
+        }
+      }
+    }
+
+    return islandCount;
+  }
+}
