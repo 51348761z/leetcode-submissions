f8d62bafeb39f21ce034b82b3c7d4467f8ff476f
51348761z <851315514@qq.com>
2026-08-12 16:25:50 +0800

    Delete my-folder directory

--- my-folder/0070-climbing-stairs/solution.ts
+++
@@ -1,7 +0,0 @@
-function climbStairs(n: number): number {
-  const dp = [1, 1];
-  for (let i = 2; i < n + 1; i++) {
-    dp[i] = dp[i - 1] + dp[i - 2];
-  }
-  return dp[n];
-}
--- my-folder/0136-single-number/solution.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
-        a = 0
-        for n in nums:
-            a ^= n
-        return a
-
-
