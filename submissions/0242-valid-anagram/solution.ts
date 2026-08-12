e4c3c1dcb6a899ad627073c7249f8942648e06b6
51348761z <hl_wong@st.gxu.edu.cn>
2026-08-12 08:49:52 +0000

    [LeetCode Sync] Runtime - 5 ms (94.42%), Memory - 57.3 MB (72.94%)

--- submissions/0242-valid-anagram/solution.ts
+++ submissions/0242-valid-anagram/solution.ts
@@ -1,13 +1,15 @@
 function isAnagram(s: string, t: string): boolean {
   if (s.length !== t.length) return false;
+  const aCode = "a".charCodeAt(0);
 
-  const counterArray = Array(26).fill(0);
+  const counts = Array<number>(26).fill(0);
   for (let index = 0; index < s.length; index++) {
-    const sCharCode = s.charCodeAt(index) - "a".charCodeAt(0);
-    const tChatCode = t.charCodeAt(index) - "a".charCodeAt(0);
-    counterArray[sCharCode] += 1;
-    counterArray[tChatCode] -= 1;
+    const sCharCode = s.charCodeAt(index) - aCode;
+    const tCharCode = t.charCodeAt(index) - aCode;
+
+    counts[sCharCode]++;
+    counts[tCharCode]--;
   }
 
-  return !counterArray.some((counter) => counter != 0);
+  return counts.every((count) => count === 0);
 }
