c062e3aba25258026c62fcbb1ba7ef2f1485bc5f
51348761z <hl_wong@st.gxu.edu.cn>
2026-08-20 08:45:40 +0000

    [LeetCode Sync] Runtime - 42 ms (37.97%), Memory - 81.5 MB (49.20%)

--- submissions/0128-longest-consecutive-sequence/solution.ts
+++ submissions/0128-longest-consecutive-sequence/solution.ts
@@ -1,13 +1,16 @@
 function longestConsecutive(nums: number[]): number {
   const numberSet = new Set(nums);
   let longest = 0;
+
   for (const n of numberSet) {
     if (numberSet.has(n - 1)) continue;
 
     let length = 1;
-    while (numberSet.has(n + length)) length++;
-
+    while (numberSet.has(n + length)) {
+      length++;
+    }
     longest = Math.max(longest, length);
   }
+
   return longest;
 }
