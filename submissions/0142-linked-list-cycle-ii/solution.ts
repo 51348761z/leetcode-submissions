5d9f86d929a0aa6de5e839fde3f9551a6242f803
51348761z <hl_wong@st.gxu.edu.cn>
2026-08-22 07:42:28 +0000

    [LeetCode Sync] Runtime - 54 ms (34.76%), Memory - 58.2 MB (46.67%)

---
+++ submissions/0142-linked-list-cycle-ii/solution.ts
@@ -0,0 +1,31 @@
+
+// class ListNode {
+//   val: number;
+//   next: ListNode | null;
+//   constructor(val?: number, next?: ListNode | null) {
+//     this.val = val === undefined ? 0 : val;
+//     this.next = next === undefined ? null : next;
+//   }
+// }
+
+function detectCycle(head: ListNode | null): ListNode | null {
+  let slow = head;
+  let fast = head;
+  while (fast && fast.next) {
+    slow = slow!.next;
+    fast = fast.next.next;
+
+    if (slow === fast) {
+      let fromHead = head;
+
+      while (fromHead !== fast) {
+        fromHead = fromHead!.next;
+        fast = fast!.next;
+      }
+
+      return fromHead;
+    }
+  }
+
+  return null;
+}
